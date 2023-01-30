4255f795b848098a0208e673e24474259c030a0f
thanhhaohoang <61105406+thanhhaohoang@users.noreply.github.com>
2023-01-30 16:29:24 +0700

    dropdown menu header
    dropdown menu header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,15 @@ import Button from '../Button';
 import './Header.scss';
 
 import logofull from './../../vbdlis-logo-white.svg';
+import UP_ICON from '../../images/arrow-left.svg';
+import DOWN_ICON from '../../images/arrow-right.svg';
+import DD_ICON1 from '../../images/checkmark.svg';
+import DD_ICON2 from '../../images/ic-star.svg';
 
 const Header = () => {
   const [active, setActive] = useState(false);
+  const [show, setShow] = useState(false);
+
   return (
     <header>
       <div className="header-content container">
@@ -26,7 +32,30 @@ const Header = () => {
         </div>
         <nav className={active ? "menu open" : "menu"}>
           <ul>
-            <li className="menu-item"><Link to='about'>Hệ thống</Link></li>
+            <li className="menu-item">
+              <Link onClick={() => setShow(show ? false : true)}>
+                Hệ thống
+                <img width={'32px'} height={'32px'} src={show ? UP_ICON : DOWN_ICON} alt="" />
+              </Link>
+              {show ? (
+                <div className="dropdown-cols">
+                  <div className="right">
+                    <ul className="dropdown">
+                      <li className="menu-item">
+                        <Link to='/websystem'>
+                          <img width={'32px'} height={'32px'} src={DD_ICON1} alt="" />
+                          Hệ thống web
+                        </Link></li>
+                      <li className="menu-item">
+                        <Link to='/desktopapp'>
+                          <img width={'32px'} height={'32px'} src={DD_ICON2} alt="" />
+                          Ứng dụng desktop
+                        </Link></li>
+                    </ul>
+                  </div>
+                </div>
+              ) : null}
+            </li>
             <li className="menu-item"><Link to='help'>Hướng dẫn</Link></li>
             <li className="menu-item"><Link to='help'>Tin tức</Link></li>
             <li className="menu-item"><Link to='help'>Liên hệ</Link></li>
@@ -37,7 +66,7 @@ const Header = () => {
           </ul>
         </nav>
       </div>
-    </header>
+    </header >
   )
 }
 export default Header;
