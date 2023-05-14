d22677c20bd237b97bed17f077d03ea334ebec3e
thanhhaohoang <61105406+thanhhaohoang@users.noreply.github.com>
2023-05-15 00:37:43 +0700

    add video tabs list

--- src/pages/Guides/Guides/GuideApp1Video.js
+++ src/pages/Guides/Guides/GuideApp1Video.js
@@ -1,90 +1,71 @@
 import './../Guides.scss';
 import React, { useEffect, useState } from 'react';
-import Button from '../../../components/Button';
-import Popup from '../../../components/Modal/Popup';
+import Tabs from '../../../components/Tabs/Tabs';
+import DataHelper, { isEmpty } from '../../../helper/data.helper';
 
 const GuideApp1Video = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [tabs, setTabs] = useState();
+  const [tabActive, setTabActive] = useState();
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    let url = './../../content/raw/app1videos.json';
+    (async () => {
+      const rs = await DataHelper.getDataJson(url);
+      if (rs) {
+        const tabs = Object.values(rs);
+        setTabs(tabs);
+        if (tabs?.length) {
+          setTabActive(tabs[0]?.id);
+        }
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (!isEmpty(tabActive)) {
+      const res = tabs.find((t) => t.id === tabActive)
+      setVideos(res?.data || []);
+    }
+  }, [tabActive]);
 
   return (
     <div className='guides-video'>
       <h2 className='guides-title'>Video hướng dẫn sử dụng</h2>
+      <div className='guides-video-tabs'>
+        <Tabs
+          menu={tabs}
+          selected={tabActive}
+          onSelect={(val) => {
+            if (tabActive === val) {
+              return;
+            }
+            else {
+              setTabActive(val)
+            }
+          }}
+        />
+      </div>
+
       <div className='video-guides-item'>
-        <div className='video-guides-card'>
-          <div className='video-thumbnail' style={{ backgroundImage: "url(" + "/guides/video-thumbnail-1.jpg" + ")" }}>
-            <div className='video-button'>
-              <Button
-                buttonStyle="button-video"
-                buttonSize="vbt-icon-only"
-                onClick={() =>
-                  setIsOpen(true)
-                }
-              >
-                <span className='ic-btn'>
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24px" height="24px">
-                    <path fill-rule="evenodd" d="M24 12.0001L5 23V1L24 12.0001Z" clip-rule="evenodd"></path>
-                  </svg>
-                </span>
-              </Button>
-              {isOpen && (
-                <Popup setIsOpen={setIsOpen} className='guides-video'>
-                  <video width="1080px" height='auto' controls >
-                    <source src="/guides/file_example.mp4" type="video/mp4" />
-                  </video>
-                </Popup>
-              )}
+        {Array.isArray(videos) && videos?.length && videos.map((vid, index) => {
+          return (
+            <div
+              key={index}
+              className='video-guides-card'
+            >
+              <div className='video-thumbnail'>
+                <iframe width="100%" height="100%" src={vid.source} title={vid.title} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" poster="/guides/video-thumbnail-1.jpg"></iframe>
+              </div>
+              <div className='video-info'>
+                <h5 className='video-title'>{vid.title}</h5>
+              </div>
             </div>
-          </div>
-          <div className='video-info'>
-            <h5 className='video-title'>Giới thiệu tác vụ cấp giấy lần đầu</h5>
-          </div>
-        </div> 
-        <div className='video-guides-card'>
-          <div className='video-thumbnail' style={{ backgroundImage: "url(" + "/guides/video-thumbnail-1.jpg" + ")" }}>
-            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/sf58kFCRqYg?mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" poster="/guides/video-thumbnail-1.jpg"></iframe>            
-          </div>
-          <div className='video-info'>
-            <h5 className='video-title'>Giới thiệu tác vụ cấp giấy lần đầu</h5>
-          </div>
-        </div>   
-        {/* <div className='video-guides-card'>
-          <div className='video-thumbnail'>
-            <video src="https://www.youtube.com/embed/sf58kFCRqYg?mute=1" poster="/guides/video-thumbnail-1.jpg"></video>           
-          </div>
-          <div className='video-info'>
-            <h5 className='video-title'>Giới thiệu tác vụ cấp giấy lần đầu</h5>
-          </div>
-        </div>     */}
-      </div>      
+          )
+        })}
+
+      </div>
     </div>
   )
 }
 export default GuideApp1Video;
-
-
-// export default function GuideApp1Video() {
-  
-//   return (
-//     <div className='guides-video'>
-//       <h3 className='guides-title'>Video hướng dẫn sử dụng</h3>
-//       <div className='video-guides-item'>
-//         <div className='video-card'>
-//           <div className='video-thumbnail'>
-//             <img src="./../content/img/news-cong-tac-cap-so-hong-2.jpg"></img>
-//           </div>  
-//           <div className='video-info'>
-//             <h3 className='video-title'>title</h3>
-//           </div>         
-//         </div>
-//         <div className='video-card'>
-//           <div className='video-thumbnail'>
-//             <img src="./../content/img/news-cong-tac-cap-so-hong-2.jpg"></img>
-//           </div>  
-//           <div className='video-info'>
-//             <h3 className='video-title'>title</h3>
-//           </div>         
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
--- src/pages/Guides/Guides/GuideApp2Video.js
+++ src/pages/Guides/Guides/GuideApp2Video.js
@@ -1,10 +1,72 @@
-import React from 'react'
+import './../Guides.scss';
+import React, { useEffect, useState } from 'react';
+import Button from '../../../components/Button';
+import Tabs from '../../../components/Tabs/Tabs';
+import DataHelper, { isEmpty } from '../../../helper/data.helper';
 
-export default function GuideApp2Video() {
+const GuideApp2Video = () => {
+  const [tabs, setTabs] = useState();
+  const [tabActive, setTabActive] = useState();
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    let url = './../../content/raw/app2videos.json';
+    (async () => {
+      const rs = await DataHelper.getDataJson(url);
+      if (rs) {
+        const tabs = Object.values(rs);
+        setTabs(tabs);
+        if (tabs?.length) {
+          setTabActive(tabs[0]?.id);
+        }
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (!isEmpty(tabActive)) {
+      const res = tabs.find((t) => t.id === tabActive)
+      setVideos(res?.data || []);
+    }
+  }, [tabActive]);
+
   return (
     <div className='guides-video'>
-      <h3 className='guides-title'>Video hướng dẫn sử dụng</h3>
-      <p>Update...</p>
+      <h2 className='guides-title'>Video hướng dẫn sử dụng</h2>
+      <div className='guides-video-tabs'>
+        <Tabs
+          menu={tabs}
+          selected={tabActive}
+          onSelect={(val) => {
+            if (tabActive === val) {
+              return;
+            }
+            else {
+              setTabActive(val)
+            }
+          }}
+        />
+      </div>
+
+      <div className='video-guides-item'>
+        {Array.isArray(videos) && videos?.length && videos.map((vid, index) => {
+          return (
+            <div
+              key={index}
+              className='video-guides-card'
+            >
+              <div className='video-thumbnail'>
+                <iframe width="100%" height="100%" src={vid.source} title={vid.title} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" poster="/guides/video-thumbnail-1.jpg"></iframe>
+              </div>
+              <div className='video-info'>
+                <h5 className='video-title'>{vid.title}</h5>
+              </div>
+            </div>
+          )
+        })}
+
+      </div>
     </div>
   )
 }
+export default GuideApp2Video;
